5c0a34f79dd634c9fd03eec4c2234fddefb29df9
Isaac <ryanwoo1989@gmail.com>
2015-01-15 17:51:45 -0500

    Update decode.cpp

--- decode.cpp
+++ decode.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
-
+#include<string>
 using namespace std;
 
+int decode(string &s){
+  if(s.empty()) return 0;
+  int pre=1, cur=1;
+  for(int i=1; i<s.length(); ++i){
+    int t=cur;
+    if(s[i-1]=='1' || s[i-1]=='2' && s[i]<='5')
+      cur+=pre;
+    pre=t;
+  }
+  return cur;
+}
+
 int main(){
-  cout<<"hello world!"<<endl;
+  string s("132493820173849029382910382");
+  cout<<decode(s)<<endl;
 }
